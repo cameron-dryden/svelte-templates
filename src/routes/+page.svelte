e2caa00ee6eac7ffca1af49874376defd8a0db78
cameron-dryden <cameronjames.dryden@gmail.com>
2024-06-14 10:54:43 +0200

    With Shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+	import Button from '$lib/shadcn/components/ui/button/button.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2 class="text-lg text-lime-500/90 hover:text-red-500">Tailwind Test</h2>
+
+<Button onclick={() => console.log('I work')}>shadcn test</Button>
