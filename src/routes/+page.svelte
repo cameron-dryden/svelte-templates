b8d6e6733ab51d0c9c57c93cd228c55d558d43ba
cameron-dryden <cameronjames.dryden@gmail.com>
2024-06-14 11:23:17 +0200

    With Iconify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
 <script>
 	import Button from '$lib/shadcn/components/ui/button/button.svelte';
+	import 'iconify-icon';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h2 class="text-lg text-lime-500/90 hover:text-red-500">Tailwind Test</h2>
+<div class="flex flex-col">
+	<h2 class="text-lg text-lime-500/90 hover:text-red-500">Tailwind Test</h2>
 
-<Button onclick={() => console.log('I work')}>shadcn test</Button>
+	<Button on:click={() => console.log('I work')}>shadcn test</Button>
+
+	<h2>Iconify Test</h2>
+	<iconify-icon
+		icon="mdi:home"
+		class="text-[48px] transition-colors text-blue-400 hover:text-purple-500"
+	></iconify-icon>
+
+	<Button>
+		<iconify-icon icon="mdi:email-outline" class="text-[16px] mr-1 mt-0.5"></iconify-icon>
+		Login with Email
+	</Button>
+</div>
